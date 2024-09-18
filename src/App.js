6e65ebd32db6a3224bd5c09d18db3a1b14c346bd
Adithya CV <adithya.cv@tcs.com>
2024-09-18 12:43:17 +0530

    das

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import Note from './components/Note';
-import NoteForm from './components/NoteForm';
-import { addNoteToDB, getNotesFromDB, deleteNoteFromDB, updateNoteInDB } from './db';
-import './App.css';
-
-
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Note from "./components/Note";
+import NoteForm from "./components/NoteForm";
+import {
+  addNoteToDB,
+  getNotesFromDB,
+  deleteNoteFromDB,
+  updateNoteInDB,
+} from "./db";
+import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -23,38 +26,40 @@ function App() {
   };
   const deleteNote = async (id) => {
     await deleteNoteFromDB(id);
-    setNotes(notes.filter(note => note.id !== id));
+    setNotes(notes.filter((note) => note.id !== id));
   };
   const editNote = async (updatedNote) => {
     await updateNoteInDB(updatedNote);
-    setNotes(notes.map(note => (note.id === updatedNote.id ? updatedNote : note)));
+    setNotes(
+      notes.map((note) => (note.id === updatedNote.id ? updatedNote : note))
+    );
   };
   return (
     <div className="App">
-
-      <div className='app-header'>
-          <h1>Notes App</h1>
+      <div className="app-header">
+        <h1>Notes App</h1>
       </div>
 
-      <div className='notes-page'>
-
-        <nav className='notes-nav'>
+      <div className="notes-page">
+        <nav className="notes-nav">
           <div className="notes-container">
-            {notes.map(note => (
-              <Note key={note.id} note={note} deleteNote={deleteNote} editNote={editNote} />
+            {notes.map((note) => (
+              <Note
+                key={note.id}
+                note={note}
+                deleteNote={deleteNote}
+                editNote={editNote}
+              />
             ))}
           </div>
         </nav>
 
-        <div className='notes-form'>
+        <div className="notes-form">
           <main>
             <NoteForm addNote={addNote} />
           </main>
         </div>
-
       </div>
-     
-
     </div>
   );
 }
